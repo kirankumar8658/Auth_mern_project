2723c4bd65274e030a060e43107e449639084ed8
kirankumar8658 <kiransingh8658@gmail.com>
2025-10-22 18:10:30 +0530

    Modified code and ready to deploy

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ import authRouter from "./routes/authRoutes.js";
 import userRouter from "./routes/userRoutes.js";
 
 const app = express();
-const port = process.env.PORT|| 5000;
+const port = process.env.PORT|| 3000;
 connectDB()
   .then(() => {
     console.log("Connected to MongoDB");
