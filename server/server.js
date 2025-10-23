5708c95f31d5871d29509dc5bac2509b0a529cc9
kiran kumar singh <kiransingh8658@gmail.com>
2025-10-23 12:48:14 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,3 +1,26 @@
+
+// server.js
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import cookieParser from "cookie-parser";
+import connectDB from "./config/db.js";
+import authRouter from "./routes/authRoutes.js";
+import userRouter from "./routes/userRoutes.js";
+
+const app = express();
+
+// Use Render PORT or fallback
+const PORT = process.env.PORT || 5000;
+
+// Connect to MongoDB
+const port = process.env.PORT|| 3000;
+connectDB()
+  .then(() => console.log("✅ Connected to MongoDB"))
+  .catch((err) => console.log("❌ MongoDB error:", err));
+
+
+// CORS middleware
 const allowedOrigins = [
   "https://auth-mern-frontend-hw7f.onrender.com",
   "http://localhost:5173"
@@ -15,3 +38,20 @@ app.use(
     credentials: true,
   })
 );
+
+
+// Middleware
+app.use(express.json());
+app.use(cookieParser());
+
+// Test route
+app.get("/", (req, res) => res.send("✅ Backend working"));
+
+// Routes
+app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
