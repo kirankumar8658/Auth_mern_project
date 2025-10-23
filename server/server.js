9aee395c523177f4ba53258f3207f3683b5d8125
kiran kumar singh <kiransingh8658@gmail.com>
2025-10-23 12:24:48 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,26 +1,17 @@
-import express from "express";
-import cors from "cors";
-import 'dotenv/config';
-import cookieParser from "cookie-parser";
-import connectDB from './config/db.js'
-import authRouter from './routes/authRoutes.js'
-import userRouter from "./routes/userRoutes.js";
+const allowedOrigins = [
+  "https://auth-mern-frontend-hw7f.onrender.com",
+  "http://localhost:5173"
+];
 
-const app = express()
-const port = process.env.PORT || 4000
-connectDB();
-
-app.use(cors({
-  origin: ['https://auth-mern-frontend-hw7f.onrender.com'], // Your frontend deployed URL
-  credentials: true,
-}));
-
-app.use(express.json());
-app.use(cookieParser());
-
-// API Endpoints
-app.get("/", (req, res) => res.send("API Working"))
-app.use('/api/auth', authRouter)
-app.use('/api/user', userRouter)
-
-app.listen(port, () => console.log(`Server started on PORT:${port}`))
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        return callback(new Error("CORS not allowed"), false);
+      }
+      return callback(null, true);
+    },
+    credentials: true,
+  })
+);
