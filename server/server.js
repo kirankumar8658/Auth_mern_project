1c6832909aeb4d485b85a9c39a65f17c487b6e8a
kiran kumar singh <kiransingh8658@gmail.com>
2025-10-23 13:23:10 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -21,23 +21,11 @@ connectDB()
 
 
 // CORS middleware
-const allowedOrigins = [
-  "https://auth-mern-frontend-hw7f.onrender.com",
-  "http://localhost:5173"
-];
+app.use(cors({
+  origin: "https://auth-mern-frontend-hw7f.onrender.com",
+  credentials: true
+}));
 
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        return callback(new Error("CORS not allowed"), false);
-      }
-      return callback(null, true);
-    },
-    credentials: true,
-  })
-);
 
 
 // Middleware
