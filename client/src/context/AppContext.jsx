9a9dcf7994294c52cfa97f1e1ca2c570be1b02fa
kirankumar8658 <kiransingh8658@gmail.com>
2026-07-16 17:14:09 +0530

    Fix AppContext warning

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -45,6 +45,7 @@ export const AppContextProvider = ({ children }) => {
 
   useEffect(() => {
     getAuthState();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const value = {
